814b075b3dd892d850300186af4f9179595c2f9b
Mar Leonard Lopez <marlen3291@gmail.com>
2016-05-20 14:41:07 -0400

    Changed connection file

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -2,16 +2,13 @@
 // This file contains the database access information. 
 // This file also establishes a connection to MySQL 
 // and selects the database.
-
 // Set the database access information as constants:
-DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PASSWORD', 'L3knight');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'D&D');
-
+DEFINE ('DB_USER', 'mll6');
+DEFINE ('DB_PASSWORD', '*******');
+DEFINE ('DB_HOST', 'sql2.njit.edu');
+DEFINE ('DB_NAME', 'mll6');
 // Make the connection:
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
 // If no connection could be made, trigger an error:
 if (!$dbc) {
 	trigger_error ('Could not connect to MySQL: ' . mysqli_connect_error() );
