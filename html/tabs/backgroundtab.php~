3639a35b53eed807ee4155bafc91d8826781bcf3
Mar Leonard Lopez <marlen3291@gmail.com>
2016-05-26 17:55:40 -0400

    I added the missing background and feature tab files.

---
+++ html/tabs/backgroundtab.php~
@@ -0,0 +1,31 @@
+<?php
+
+//Page 3 Start
+  	echo '
+  	<div id="tabs-3">
+   ';
+   
+   //Page 3
+
+	
+	echo '<div id="background2">';
+		echo "<p><b><u>Age</u></b>: $age</p>";
+		echo "<p><b><u>Height</u></b>: $height</p>";
+		echo "<p><b><u>Weight</u></b>: $weight</p><br>";
+		echo "<p><b><u>Eyes</u></b>: $eyes</p>";
+		echo "<p><b><u>Skin</u></b>: $skin</p>";
+		echo "<p><b><u>Hair</u></b>: $hair</p>";
+	
+		echo "<p><b><u>Organization</u></b>: $organization</p>";
+		echo "<p><b><u>Rank</u></b>: $rank</p><br>";
+	
+		echo "<p><b><u>Backstory</u></b>:"	.	nl2br($backstory)	.	"</p><br>";
+	
+		
+   echo '</div><br>';
+   //Page 3 End
+   echo '
+  	</div>
+  	';
+  	
+?>
---
+++ html/tabs/featuretab.php
@@ -0,0 +1,83 @@
+<?php
+
+//Page 7 Start
+  	echo '
+  	<div id="tabs-7">
+   ';
+   
+   //Select everything from features
+	echo '<h1><b>Features</b></h1>';
+	
+	$fk = "SELECT * FROM features WHERE character_id=$character_id";
+	
+	
+	$fki = mysqli_query ($dbc, $fk) or trigger_error("Query: $c\n<br />MySQL Error: " . mysqli_error($dbc));
+	
+	if(mysqli_num_rows($fki) !=0){
+	echo '<table id="myTablefeature" class="display">';
+	echo "<thead>";
+		echo "<tr>";
+	
+	
+		echo "<th>Name</th>";
+		echo "<th>Description</th>";
+		if (isset($_SESSION['user_id']) && $public_viewing != "Yes"){
+		echo "<th>Feature Link</th>";
+		}
+			
+		echo "</tr>";
+	echo "</head>";	
+	echo "<tbody>";
+	
+	while($row = mysqli_fetch_array($fki))
+	{
+	$feature_id = $row["feature_id"];
+	$feature_name = $row["feature_name"];
+	$feature_description = $row["feature_description"];
+	
+		
+	echo "<tr>";
+
+		echo "<td>" .	$feature_name	.	"</td>"	;
+		echo "<td>" .	nl2br($feature_description)	.	"</td>"	;
+	
+		if (isset($_SESSION['user_id']) && $public_viewing != "Yes"){
+			
+		echo 
+		
+		"<td>
+		
+			<a href='feature_view.php?feature_id=$feature_id'>View Feature</a> 
+		</td>";
+		}
+	}
+	echo "</tbody>";
+	echo "</table>";
+	}
+	else{echo '<p>No Feature Written Yet</p>';}
+	
+	if (isset($_SESSION['user_id']) && $public_viewing != "Yes"){
+		
+	echo ' 
+	
+		<form action="create_feature.php" method="get">
+
+				<input type="hidden" name="character_id" value='	.	$character_id . '>
+		
+				<input type="submit" name="submit" value="Acquire A Feature" />
+		
+		</form>
+	';
+		
+	}
+	echo '<br>';
+	//Feature End
+	
+	
+   //Page 7 End
+   echo '
+  	</div>
+  	';
+
+
+?>
